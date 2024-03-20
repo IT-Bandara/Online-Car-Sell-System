ba668e0c0f7591881f42809d109b6c6dcd5e4415
Isuru <isurutharakabandara@gmail.com>
2024-03-20 10:06:05 +0530

    Set the advertisements

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Page/Wellcome page/Navbar';
 import WelcomeBody from './Page/Wellcome page/WelcomeBody';
+import Advertisement from './Component/Advertisement/Advertisement';
+
 
 function App() {
   return (
     <>
       <Navbar/>
       <WelcomeBody/>
+      
+      <Advertisement size='900x250'>
+        <img src='welcomeImage.jpg'/>
+      </Advertisement>
+      <Advertisement size='600x150'>
+        <img src='welcomeImage.jpg'/>
+      </Advertisement>
+      <Advertisement size='150x500'>
+        <img src='welcomeImage.jpg'/>
+      </Advertisement>
     </>
   );
 }
---
+++ src/Component/Advertisement/Advertisement.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Advertisement.css'
+
+function Advertisement(props) {
+  return (
+    <div className={`Advertisement-${props.size}`}>
+      <div className="image-container">
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+export default Advertisement
